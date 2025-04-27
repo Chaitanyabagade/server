a30dc28e6063627c6094eb304648ea3bdd0f6bc3
ChaitanyaBagade <chaitanyabagade59@gmail.com>
2025-04-27 16:51:19 +0530

    adding the update logic

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ db.connect((err) => {
     console.error('Database connection failed: ' + err.stack);
     return;
   }
-  console.log('Connected to database.');
+  console.log('Connected to database successfully.');
 });
 
 
